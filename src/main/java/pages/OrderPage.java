a638c8ad7b9d1958786be67ab43baa09dcd62c2c
TT <iloewen79@gmx.de>
2024-11-15 20:24:11 +0100

    Completed filling out the first page of the order form

--- src/main/java/pages/OrderPage.java
+++ src/main/java/pages/OrderPage.java
@@ -1,4 +1,3 @@
-// OrderPage.java в пакете pages
 package pages;
 
 import org.openqa.selenium.By;
@@ -6,8 +5,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.openqa.selenium.JavascriptExecutor;
-
 import java.time.Duration;
 
 public class OrderPage {
@@ -18,15 +15,60 @@ public class OrderPage {
         this.driver = driver;
     }
 
-
-    // Локатор для поля "Имя"
+    // Локатор и метод для "Имя"
     public By nameField = By.cssSelector("input[type='text'][placeholder='* Имя']");
-
-
-    // Метод для ввода данных в поля
     public void enterName(String name) {
         driver.findElement(nameField).sendKeys(name);
+    }
 
+    // Локатор и метод для "Фамилия"
+    public By surnameField = By.cssSelector("input[type='text'][placeholder='* Фамилия']");
+    public void enterSurname(String surname) {
+        driver.findElement(surnameField).sendKeys(surname);
+    }
 
+    // Локатор и метод для "Адрес"
+    public By addressField = By.cssSelector("div.Order_Form__17u6u > div:nth-child(3) > input");
+    public void enterAddress(String address) {
+        driver.findElement(addressField).sendKeys(address);
+    }
+
+    // Локатор и метод для "Телефон"
+    public By phoneField = By.cssSelector("div.Order_Form__17u6u > div:nth-child(5) > input");
+    public void enterPhone(String phone) {
+        driver.findElement(phoneField).sendKeys(phone);
+    }
+
+    // Локатор для поля ввода станции
+    public By stationField = By.cssSelector("input[placeholder='* Станция метро']");
+
+    // Метод для выбора станции
+    public void selectStation(String station) {
+        // Нажимаем на поле ввода станции
+        WebElement stationInput = driver.findElement(stationField);
+        stationInput.click();
+
+        // Вводим название станции
+        stationInput.sendKeys(station);
+
+        // Ждём появления выпадающего списка с нужной станцией
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        By stationOption = By.xpath("//div[contains(@class, 'select-search__row') and text()='" + station + "']");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(stationOption));
+
+        // Кликаем по станции
+        driver.findElement(stationOption).click();
+
+        // Проверяем, что поле ввода содержит выбранную станцию
+        wait.until(ExpectedConditions.attributeToBe(stationField, "value", station));
+    }
+
+    // Локатор и метод для нажатия кнопки "Далее"
+    public By nextButton = By.xpath("//*[@id='root']/div/div[2]/div[3]/button");
+    public void clickNextButton() {
+        // Нажимаем на кнопку "Далее"
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(120));
+        wait.until(ExpectedConditions.elementToBeClickable(nextButton));
+        driver.findElement(nextButton).click();
     }
 }
--- src/test/java/tests/OrderPageTest.java
+++ src/test/java/tests/OrderPageTest.java
@@ -1,10 +1,13 @@
 package tests;
 
 import org.junit.Test;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.MainPage;
 import pages.OrderPage;
 import static org.junit.Assert.assertEquals;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.time.Duration;
 
 public class OrderPageTest extends TestsSetUp {
     private MainPage mainPage;
@@ -17,9 +20,28 @@ public class OrderPageTest extends TestsSetUp {
 
         // Нажимаем на верхнюю кнопку "Заказать"
         mainPage.clickUpperOrderButton();
+                  // Нажимаем на нижнюю кнопку "Заказать" - можно подключать через удаление
+         //перед строкой вызова следующего метода, но тогда метод выше надо отключить.
+                   // только одна их кнопок за флоу должна работать!
+        //mainPage.clickLowerOrderButton();
 
-        // Проверяем, что поле "Имя" появляется и вводим имя
-        wait.until(ExpectedConditions.visibilityOfElementLocated(orderPage.nameField));
-        orderPage.enterName("Test Name");
+
+        // Делаем ввод в поле имя после явного появления элемента
+        wait.until(ExpectedConditions.visibilityOfElementLocated(orderPage.phoneField));
+        orderPage.enterName("Игорь");
+        orderPage.enterSurname("Лёвэн");
+        orderPage.enterAddress("вапыпыавыаыва");
+        orderPage.enterPhone("245624563546");
+        orderPage.selectStation("Сокольники");
+        orderPage.clickNextButton();
+
+
+
+
+
+
+
+
+
     }
 }
