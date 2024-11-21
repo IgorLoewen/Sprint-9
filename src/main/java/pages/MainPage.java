23ef94d8f4567cc43ce31f52d2424c8a76c4be06
TT <iloewen79@gmx.de>
2024-11-21 14:26:06 +0100

    Отсюда идёт оптимизация локаторов

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -11,35 +11,47 @@ import java.time.Duration;
 public class MainPage {
     private WebDriver driver;
 
+    // ========================= Константы =========================
     // Статическая переменная для базового URL
     public static final String BASE_URL = "https://qa-scooter.praktikum-services.ru/";
 
-    // Конструктор
+    // ========================= Конструктор =========================
     public MainPage(WebDriver driver) {
         this.driver = driver;
     }
 
-    // Локаторы для верхней и нижней кнопок ЗАКАЗАТЬ
-    private By upperOrderButton = By.xpath("(//button[contains(@class, 'Button_Button__ra12g')])[1]");
-    private By lowerOrderButton = By.xpath("//*[@id='root']/div/div/div[4]/div[2]/div[5]/button");
+    // ========================= Локаторы =========================
+    // Кнопки "Заказать"
+    private static final By upperOrderButton = By.xpath("(//button[contains(@class, 'Button_Button__ra12g')])[1]");
+    private static final By lowerOrderButton = By.xpath("//*[@id='root']/div/div/div[4]/div[2]/div[5]/button");
 
-    // Локатор для кнопки вопроса по индексу
-    private By questionButton(int questionIndex) {
+    // Локаторы FAQ
+    private static By questionButton(int questionIndex) {
         return By.id("accordion__heading-" + questionIndex);
     }
 
-    // Локатор для текстов ответов FAQ
-    private By answerLocator(int questionIndex) {
+    private static By answerLocator(int questionIndex) {
         return By.id("accordion__panel-" + questionIndex);
     }
 
-    // Метод для прокрутки к элементу
+    // Логотипы
+    public static final By scooterLogo = By.xpath("//img[@alt='Scooter']");
+    public static final By yandexLogo = By.xpath("//img[@alt='Yandex']");
+
+    // Локаторы статуса заказа
+    public static final By STATUS_ORDER_BUTTON = By.xpath("//button[text()='Статус заказа']");
+    public static final By ORDER_NUMBER_INPUT = By.xpath("//input[@placeholder='Введите номер заказа']");
+    public static final By GO_BUTTON = By.xpath("/html/body/div/div/div/div[1]/div[3]/button");
+    public static final By ORDER_NOT_FOUND_MESSAGE = By.cssSelector("#root > div > div.Track_Content__St6Kn > div.Track_NotFound__6oaoY");
+
+    // ========================= Методы =========================
+
+    // ---------- Общие методы ----------
     public void scrollToElement(By locator) {
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("arguments[0].scrollIntoView(true);", driver.findElement(locator));
     }
 
-    // Универсальный метод для клика по кнопке
     public void clickButton(By locator) {
         scrollToElement(locator);
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
@@ -47,7 +59,7 @@ public class MainPage {
         driver.findElement(locator).click();
     }
 
-    // Методы для клика по кнопкам "Заказать"
+    // ---------- Методы для кнопок "Заказать" ----------
     public void clickUpperOrderButton() {
         clickButton(upperOrderButton);
     }
@@ -56,70 +68,45 @@ public class MainPage {
         clickButton(lowerOrderButton);
     }
 
-    // Метод для клика по кнопке вопроса
+    // ---------- Методы для FAQ ----------
     public void clickQuestionButton(int questionNumber) {
         clickButton(questionButton(questionNumber));
     }
 
-    // Метод для получения текста ответа на вопрос
     public String getAnswerText(int questionNumber) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.visibilityOfElementLocated(answerLocator(questionNumber)));
         return driver.findElement(answerLocator(questionNumber)).getText();
     }
 
-    // Метод для получения текста вопроса
     public String getQuestionText(int questionNumber) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.visibilityOfElementLocated(questionButton(questionNumber)));
         return driver.findElement(questionButton(questionNumber)).getText();
     }
 
-    // Локатор для логотипа "Самокат"
-    public static By scooterLogo = By.xpath("//img[@alt='Scooter']");
-
-    // Локатор для логотипа "Яндекс"
-    public static By yandexLogo = By.xpath("//img[@alt='Yandex']");
-
-    // Локатор для кнопки "Статус заказа"
-    public static final By STATUS_ORDER_BUTTON = By.xpath("//button[text()='Статус заказа']");
-
-    // Локатор для поля ввода номера заказа
-    public static final By ORDER_NUMBER_INPUT = By.xpath("//input[@placeholder='Введите номер заказа']");
-
-    // Локатор для кнопки "Go"
-    public static final By GO_BUTTON = By.xpath("/html/body/div/div/div/div[1]/div[3]/button");
-
-    // Локатор для сообщения об отсутствии заказа
-    public static final By ORDER_NOT_FOUND_MESSAGE = By.cssSelector("#root > div > div.Track_Content__St6Kn > div.Track_NotFound__6oaoY");
-
-
-    // Метод для нажатия на кнопку "Статус заказа"
+    // ---------- Методы для статуса заказа ----------
     public void clickStatusOrderButton() {
         new WebDriverWait(driver, Duration.ofSeconds(10))
                 .until(ExpectedConditions.elementToBeClickable(STATUS_ORDER_BUTTON))
                 .click();
     }
 
-    // Метод для ввода номера заказа в поле
     public void enterOrderNumber(String orderNumber) {
         new WebDriverWait(driver, Duration.ofSeconds(10))
                 .until(ExpectedConditions.visibilityOfElementLocated(ORDER_NUMBER_INPUT))
                 .sendKeys(orderNumber);
     }
 
-    // Метод для нажатия на кнопку "Go"
     public void clickGoButton() {
         new WebDriverWait(driver, Duration.ofSeconds(10))
                 .until(ExpectedConditions.elementToBeClickable(GO_BUTTON))
                 .click();
     }
 
-    // Метод для проверки отображения сообщения "Такого заказа нет"
     public boolean isOrderNotFoundMessageVisible() {
         return new WebDriverWait(driver, Duration.ofSeconds(10))
                 .until(ExpectedConditions.visibilityOfElementLocated(ORDER_NOT_FOUND_MESSAGE))
                 .isDisplayed();
     }
-
 }
--- src/main/java/pages/OrderPage.java
+++ src/main/java/pages/OrderPage.java
@@ -3,46 +3,53 @@ package pages;
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.time.Duration;
 
 public class OrderPage {
     private WebDriver driver;
 
+    // ========================= Константы =========================
     // Статическая переменная для базового URL
     public static final String Order_URL = "https://qa-scooter.praktikum-services.ru/order";
 
-    // Конструктор для инициализации драйвера
+    // ========================= Конструктор =========================
     public OrderPage(WebDriver driver) {
         this.driver = driver;
     }
 
     // ========================= Локаторы =========================
-
     // Поля для ввода данных
-    private By nameField = By.cssSelector("input[type='text'][placeholder='* Имя']");
-    private By surnameField = By.cssSelector("input[type='text'][placeholder='* Фамилия']");
-    private By addressField = By.cssSelector("div.Order_Form__17u6u > div:nth-child(3) > input");
-    private By phoneField = By.cssSelector("div.Order_Form__17u6u > div:nth-child(5) > input");
-    private By stationField = By.cssSelector("input[placeholder='* Станция метро']");
+    private static final By nameField = By.cssSelector("input[type='text'][placeholder='* Имя']");
+    private static final By surnameField = By.cssSelector("input[type='text'][placeholder='* Фамилия']");
+    private static final By addressField = By.cssSelector("div.Order_Form__17u6u > div:nth-child(3) > input");
+    private static final By phoneField = By.cssSelector("div.Order_Form__17u6u > div:nth-child(5) > input");
+    private static final By stationField = By.cssSelector("input[placeholder='* Станция метро']");
 
     // Кнопки
-    private By nextButton = By.xpath("//*[@id='root']/div/div[2]/div[3]/button");
-    private By placeOrderButton = By.xpath("/html/body/div/div/div[2]/div[3]/button[2]");
-    private By confirmOrderButton = By.xpath("//button[text()='Да']");
+    private static final By nextButton = By.xpath("//*[@id='root']/div/div[2]/div[3]/button");
+    private static final By placeOrderButton = By.xpath("/html/body/div/div/div[2]/div[3]/button[2]");
+    private static final By confirmOrderButton = By.xpath("//button[text()='Да']");
 
     // Поле для ввода даты аренды
-    private By calendarDateField = By.cssSelector("div > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div > input");
+    private static final By calendarDateField = By.cssSelector("div > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div > input");
 
     // Срок аренды
-    private By rentalTermField = By.xpath("//div[@class='Dropdown-placeholder' and contains(text(), 'Срок аренды')]");
-    private By rentalOptionThreeDays = By.xpath("//div[text()='трое суток']");
+    private static final By rentalTermField = By.xpath("//div[@class='Dropdown-placeholder' and contains(text(), 'Срок аренды')]");
+    private static final By rentalOptionThreeDays = By.xpath("//div[text()='трое суток']");
 
     // Заголовок окна подтверждения заказа
-    private By orderConfirmationHeader = By.xpath("//div[contains(@class, 'Order_ModalHeader') and text()='Заказ оформлен']");
+    private static final By orderConfirmationHeader = By.xpath("//div[contains(@class, 'Order_ModalHeader') and text()='Заказ оформлен']");
+
+    // Локаторы для текстов ошибок
+    private static final By NAME_ERROR = By.xpath("//div[@class='Input_ErrorMessage__3HvIb Input_Visible___syz6' and text()='Введите корректное имя']");
+    private static final By SURNAME_ERROR = By.xpath("//div[@class='Input_ErrorMessage__3HvIb Input_Visible___syz6' and text()='Введите корректную фамилию']");
+    private static final By ADDRESS_ERROR = By.xpath("//div[@class='Input_ErrorMessage__3HvIb Input_Visible___syz6' and text()='Введите корректный адрес']");
+    private static final By PHONE_ERROR = By.xpath("//div[@class='Input_ErrorMessage__3HvIb Input_Visible___syz6' and text()='Введите корректный номер']");
 
     // ========================= Методы =========================
 
-    // Ввод данных в поля
+    // ---------- Ввод данных в поля ----------
     public void enterName(String name) {
         driver.findElement(nameField).sendKeys(name);
     }
@@ -70,7 +77,7 @@ public class OrderPage {
         wait.until(ExpectedConditions.attributeToBe(stationField, "value", station));
     }
 
-    // Работа с кнопками
+    // ---------- Работа с кнопками ----------
     public void clickNextButton() {
         driver.findElement(nextButton).click();
     }
@@ -87,14 +94,14 @@ public class OrderPage {
         driver.findElement(confirmOrderButton).click();
     }
 
-    // Работа с датой аренды
+    // ---------- Работа с датой аренды ----------
     public void enterCalendarDate(String date) {
         WebElement calendarField = driver.findElement(calendarDateField);
         calendarField.click();
         calendarField.sendKeys(date);
         calendarField.sendKeys(Keys.ENTER);
     }
-    // Выбор срока аренды
+
     public void selectRentalTermThreeDays() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement rentalField = wait.until(ExpectedConditions.elementToBeClickable(rentalTermField));
@@ -103,19 +110,12 @@ public class OrderPage {
         optionThreeDays.click();
     }
 
-    // Проверка появления окна подтверждения
+    // ---------- Проверка ----------
     public boolean isOrderConfirmationHeaderVisible() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         return wait.until(ExpectedConditions.visibilityOfElementLocated(orderConfirmationHeader)).isDisplayed();
     }
 
-    // Локаторы для текстов ошибок
-    private static final By NAME_ERROR = By.xpath("//div[@class='Input_ErrorMessage__3HvIb Input_Visible___syz6' and text()='Введите корректное имя']");
-    private static final By SURNAME_ERROR = By.xpath("//div[@class='Input_ErrorMessage__3HvIb Input_Visible___syz6' and text()='Введите корректную фамилию']");
-    private static final By ADDRESS_ERROR = By.xpath("//div[@class='Input_ErrorMessage__3HvIb Input_Visible___syz6' and text()='Введите корректный адрес']");
-    private static final By PHONE_ERROR = By.xpath("//div[@class='Input_ErrorMessage__3HvIb Input_Visible___syz6' and text()='Введите корректный номер']");
-
-    // Метод для проверки видимости ошибок
     public boolean isErrorVisible(By locator) {
         try {
             return driver.findElement(locator).isDisplayed();
@@ -124,12 +124,10 @@ public class OrderPage {
         }
     }
 
-    // Метод для проверки ошибок всех полей
     public boolean areAllErrorsVisible() {
         return isErrorVisible(NAME_ERROR) &&
                 isErrorVisible(SURNAME_ERROR) &&
                 isErrorVisible(ADDRESS_ERROR) &&
                 isErrorVisible(PHONE_ERROR);
     }
-
 }
